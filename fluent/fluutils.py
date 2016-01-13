ba0f8d9a4316abde5c81f14f644072639ceb73c1
drwindstrom <winstroth@tfd.uni-hannover.de>
2016-01-13 17:03:02 +0100

    Recursive extraction of cl, cd and cm is now implemented

--- fluent/fluutils.py
+++ fluent/fluutils.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import fnmatch
+import numpy as np
 import os
+import pandas as pd
 import pickle
 
 
@@ -141,6 +144,7 @@ def split_fname(fname):
 
     """
     fname, f_ext = os.path.splitext(fname)
+    f_ext = f_ext.replace('.', '')
     fname_split = fname.split('_', 4)
     if len(fname_split) < 4:
         raise ValueError('The supplied string is not in the right format!')
@@ -151,7 +155,14 @@ def split_fname(fname):
     splits['aoa'] = fname_split[3]
     if len(fname_split) == 5:
         splits['optional'] = fname_split[4]
-    aoa = float(fname_split[3][1:6])
+    if fname_split[3][0] == 'm':
+        aoa = -float(fname_split[3][1:6])
+    elif fname_split[3][0] == 'p':
+        aoa = float(fname_split[3][1:6])
+    else:
+        raise ValueError('The definition of aoa in the fname must be of format'
+                         ' "m00.00" or "p00.00"')
+    splits['ext'] = f_ext
     return splits, aoa
 
 
@@ -170,3 +181,133 @@ def create_sim_name(nairfoil, ntype, nsetup, aoa, optional=None):
         sim_name = '{}_{}_{}_p{:0=5.2f}{}'.format(nairfoil, nsetup, ntype,
                                                   abs(aoa), optional)
     return sim_name
+
+
+def get_coeff(fname):
+        """Loads the convergence history of the (cd, cl, cm depending on fname)
+        coefficient from fname and return the convergence history and last
+        entry from convergence history.
+
+        """
+        data = np.loadtxt(fname, skiprows=2, usecols=(1,))
+        # Last entry in numpy array is the correct value
+        last_entry = data[-1]
+        return last_entry, data
+
+
+def find_files(top_dir, pattern):
+    """Recursively finds all files in top_dir and all subdirectories which
+    match pattern.
+
+    Pattern uses unix shell-style wildcards and the following patterns are
+    supported:
+    *  matches everything
+    ?  matches any single character
+    [seq]  matches any character in seq
+    [!seq]  matches any character not in seq
+
+    Returns:
+        list(list): [[filename, full path to filename]]
+
+    """
+    matches = []
+    for root, dirs, files in os.walk(top_dir):
+        for filename in fnmatch.filter(files, pattern):
+            matches.append([filename, os.path.join(root, filename)])
+    return matches
+
+
+def get_polar(top_dir, npolar='cl', pattern='*.cl'):
+    """Returns polars extracted from fluent files which match pattern.
+
+    This functions recursively scans the directory top_dir and all it's
+    subdirectories for files that match pattern. From these files the last
+    value of the data series is extracted and returned. The corresponding
+    angle of attack is extracted from the filename. The data is organized in
+    a dictionary. The keys identify the different simulations/polars and the
+    values are pandas DataFrames which hold the polar data.
+
+    Args:
+        top_dir (str): Path to the root directory from where to start scanning
+            for files that match pattern.
+        npolar (str): Column name used for the pandas DataFrame to represent
+            the data from the files.
+        pattern (str): Unix shell-style wildcards to match file pattern. The
+            following patterns are supported:
+                *  matches everything
+                ?  matches any single character
+                [seq]  matches any character in seq
+                [!seq]  matches any character not in seq
+
+    Returns:
+        dict(pd.DataFrame): The key identifies the simulation setup and the
+            value holds the polar data inside a pandas DataFrame.
+
+    """
+    # Recursively find all files that match pattern
+    matched_files = find_files(top_dir, pattern)
+    # Loop over all matched files and extract the aoa and the coefficient
+    polars = {}
+    for cur_file in matched_files:
+        fname_parts, aoa = split_fname(cur_file[0])
+        coeff_val, coeff_hist = get_coeff(cur_file[1])
+        polar_id = '{}_{}'.format(fname_parts['nairfoil'],
+                                  fname_parts['nsetup'])
+        if polar_id not in polars:
+            polars[polar_id] = []
+        polars[polar_id].append([aoa, coeff_val])
+    # Convert data to pandas Dataframe
+    polars_df = {}
+    for key, value in polars.iteritems():
+        polars_df[key] = pd.DataFrame(data=value, columns=['aoa', npolar])
+        # Order by aoa
+        polars_df[key].sort_values('aoa', inplace=True, ascending=True)
+        # Set ordered column of aoa as index
+        polars_df[key].index = polars_df[key].aoa
+    return polars_df
+
+
+def get_clcdcm(top_dir):
+    """Returns all polars found in top_dir and it's subdirectories.
+
+    This function uses get_polar() to find all files containing simulation
+    data for cl, cd and cm. We start at top_dir and also include all
+    subdirectories. The returned polars are organized in pandas DataFrames.
+
+    Args:
+        top_dir (str): Path to root directory from where to start scanning
+
+    Returns:
+        dict(pd.DataFrame): The keys are the names of the simulation setups and
+            the values are pandas Dataframes.
+    """
+    # Get all polars for cl, cd and cm
+    cl_polars = get_polar(top_dir, npolar='cl', pattern='*.cl')
+    cd_polars = get_polar(top_dir, npolar='cd', pattern='*.cd')
+    cm_polars = get_polar(top_dir, npolar='cm', pattern='*.cm')
+    # Get all keys and put them in a set
+    cl_keys = cl_polars.keys()
+    cd_keys = cd_polars.keys()
+    cm_keys = cm_polars.keys()
+    keys = set(cl_keys)
+    keys.update(cd_keys)
+    keys.update(cm_keys)
+    # Combine polars of cl, cd and cm which have the same simulation setup
+    polars_df = {}
+    for polar_id in keys:
+        # Define a new sorted index which includes all angles of attack.
+        # This is necessary to handle missing data.
+        index_set = set(cl_polars[polar_id].index)
+        index_set.update(cd_polars[polar_id].index)
+        index_set.update(cm_polars[polar_id].index)
+        new_index = list(index_set)
+        new_index = sorted(new_index)
+        # Create new DataFrame which includes cl, cd and cm
+        polars_df[polar_id] = pd.DataFrame(index=new_index)
+        if polar_id in cl_polars:
+            polars_df[polar_id]['cl'] = cl_polars[polar_id].cl
+        if polar_id in cd_polars:
+            polars_df[polar_id]['cd'] = cd_polars[polar_id].cd
+        if polar_id in cm_polars:
+            polars_df[polar_id]['cm'] = cm_polars[polar_id].cm
+    return polars_df
